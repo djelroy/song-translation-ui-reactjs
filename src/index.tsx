44f1abb5113c71398d9e0974427d56be433903e8
DJ Elroy <djelroys@gmail.com>
2019-08-04 18:34:39 -0400

    Type argument with HTMLElement

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './router';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
